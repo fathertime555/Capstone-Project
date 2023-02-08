a2549bcdbe915faf38e249ba6b0dd923a8823257
Alex Levasseur <chatterboxlevasseur@gmail.com>
2023-02-08 14:31:01 -0800

    Separated the serializers for get and post and changed homepage into data

--- listings/serializers.py
+++ listings/serializers.py
@@ -2,22 +2,33 @@ from rest_framework import serializers
 from listings.models import Listing, Item
 
 
-class ListingSerializer(serializers.ModelSerializer):
+class ListingSerializerPost(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = "__all__"
+
+class ListingSerializerGet(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = [
-            'pk',
             'title',
             'listing_main_photo',
             'description',
-            'location'
+            'location',
+            'lat',
+            'lng'
         ]
 
-class ItemSerializer(serializers.ModelSerializer):
+
+class ItemSerializerPost(serializers.ModelSerializer):
+   class Meta:
+        model = Item
+        fields = "__all__"
+
+class ItemSerializerGet(serializers.ModelSerializer):
    class Meta:
         model = Item
         fields = [
-            'pk',
             'name',
             'item_main_photo',
             'description',
