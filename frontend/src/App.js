be7b02572e2b4232e1378c518d90287bb48d7c45
Nozdromu <yileiding1990@gmail.com>
2023-02-15 14:51:46 -0800

    basic chage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Api from './Api'
+import { Button, Col, Container, Tab, Tabs, Row } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const [listingsdata, setlistingtext] = useState('no data')
   console.log(Api);
   return (
-    <div>
-      <button onClick={() => {
-        Api.data.getlist().then(data => {
-          console.log(data);
-        })
-      }}>get listings list test</button>
-      <button onClick={() => {
-      Api.data.getitems().then(data => {
-        console.log(data);
-      })
-    }}>get listings items test</button>
-    </div>
+    <Container>
+      <Tabs>
+        <Tab eventKey={'user'} title={'User'}>
+          <div>user</div>
+        </Tab>
+        <Tab eventKey={'listings'} title={'Listings'}>
+          <Row>
+            <Col>
+              <Button onClick={() => {
+                Api.data.getlist().then(data => {
+                  setlistingtext(JSON.stringify(data.data))
+                  console.log(data);
+                })
+              }}>get listings list test</Button>
+            </Col>
+            <Col>
+              <Button onClick={() => {
+                Api.data.getitems().then(data => {
+                  setlistingtext(JSON.stringify(data.data));
+                  console.log(data);
+                })
+              }}>get listings items test</Button>
+            </Col>
+          </Row>
+          <Row>
+            {listingsdata}
+          </Row>
+
+        </Tab>
+      </Tabs>
+
+    </Container>
 
   );
 }
