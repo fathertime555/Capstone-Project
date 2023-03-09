47d62834264927cce5fd9b1965b52ee33427c1aa
Nozdromu <yileiding1990@gmail.com>
2023-03-08 20:25:41 -0800

    add media_root

--- SpiffoList/settings.py
+++ SpiffoList/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,7 +64,7 @@ ROOT_URLCONF = "SpiffoList.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -174,5 +174,8 @@ CHANNEL_LAYERS = {
     }
 GOOGLE_API_KEY = 'AIzaSyBrIdKaE4vk0uAbmKnlVxJm6lcA4XDwjhw'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "build")
+MEDIA_URL = '/media/'
 
