7e92dc1b261e18fec43c1c0b69ea769b297226a8
lambdacore <jonathanwh93@gmail.com>
2023-03-09 10:15:43 -0800

    removed password from the mainuser serializer

--- users/serializers.py
+++ users/serializers.py
@@ -11,7 +11,6 @@ class MainUserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "email",
-            "password",
             "address_line_1",
             "address_line_2",
             "city",
