294d1df1ec23b66415ecf16cbb3a6eca685c05b7
Nozdromu <yileiding1990@gmail.com>
2023-03-13 21:48:45 -0700

    set up static url

--- SpiffoList/settings.py
+++ SpiffoList/settings.py
@@ -135,10 +135,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -180,6 +177,10 @@ CHANNEL_LAYERS = {
     
 GOOGLE_API_KEY = 'AIzaSyBrIdKaE4vk0uAbmKnlVxJm6lcA4XDwjhw'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static/')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "build/media/")
