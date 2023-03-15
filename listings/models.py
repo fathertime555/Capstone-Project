0bc0af24c5a29da39312f7e84859b0b5ef9d1a92
Alex Levasseur <chatterboxlevasseur@gmail.com>
2023-03-14 17:30:41 -0700

    fixing photo upload

--- listings/models.py
+++ listings/models.py
@@ -13,7 +13,7 @@ def item_directory_path (instance, filename):
 
 class Listing(models.Model):
 	title = models.CharField(max_length = 50)
-	listing_main_photo = models.ImageField(upload_to = listing_directory_path(), blank = True, null = True)
+	listing_main_photo = models.ImageField(upload_to = listing_directory_path, blank = True, null = True)
 	listing_photo_url = models.FilePathField(null = True, blank = True)
 	description = models.TextField(null = True, blank = True)
 	location = models.CharField(max_length = 150)
@@ -28,7 +28,7 @@ class Listing(models.Model):
 class Item(models.Model):
 	name = models.CharField(max_length = 150)
 	item_photo_url = models.FilePathField(null = True, blank = True)
-	item_photo = models.ImageField(upload_to = item_directory_path(), blank = True, null = True)
+	item_photo = models.ImageField(upload_to = item_directory_path, blank = True, null = True)
 	description = models.TextField(null = True, blank = True)
 	quantity = models.IntegerField()
 	price = models.FloatField()
